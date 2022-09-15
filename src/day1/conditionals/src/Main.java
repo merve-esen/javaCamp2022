a82ce49568d674b97ff3466f3aa6a74adfce0c35
merve-esen <merveesen834@gmail.com>
2022-09-15 23:36:29 +0300

    Day 1 - Assignment 2

---
+++ src/day1/conditionals/src/Main.java
@@ -0,0 +1,16 @@
+package day1.conditionals.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int sayi = 23;
+		if (sayi < 20) {
+			System.out.println("Sayý 20'den küçüktür.");
+		} else if (sayi == 20) {
+			System.out.println("Sayý 20'ye eþittir.");
+		} else {
+			System.out.println("Sayý 20'den büyüktür.");
+		}
+	}
+
+}
