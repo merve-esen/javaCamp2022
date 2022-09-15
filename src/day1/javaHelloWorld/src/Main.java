eb0118eaf1752e6cafbb066912c3939e3eda6a00
merve-esen <merveesen834@gmail.com>
2022-09-15 22:36:34 +0300

    Day 1 - Assignment 2

---
+++ src/day1/javaHelloWorld/src/Main.java
@@ -0,0 +1,10 @@
+package day1.javaHelloWorld.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+		System.out.println("Merhaba Java");
+	}
+
+}
