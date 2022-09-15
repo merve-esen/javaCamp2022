dfbf12641ba2477d25169ef3e3650c4fb2543d1e
merve-esen <merveesen834@gmail.com>
2022-09-15 23:27:17 +0300

    Day 1 - Assignment 2

---
+++ src/day1/dataTypes/src/Main.java
@@ -0,0 +1,12 @@
+package day1.dataTypes.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		double sayi = 12.34;
+		sayi = -234;
+		char karakter = 'M';
+		boolean isActive = true;
+	}
+
+}
