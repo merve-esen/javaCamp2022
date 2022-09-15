6d6eeb7cb6dfffffe71f4c04cec37b48c43e7373
merve-esen <merveesen834@gmail.com>
2022-09-15 23:13:12 +0300

    Day 1 - Assignment 2

---
+++ src/day1/variables/Main.java
@@ -0,0 +1,13 @@
+package day1.variables;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int ogrenciSayisi = 12;
+		String mesaj = "Öðrenci sayýsý: ";
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öðrenci sayýsý: " + ogrenciSayisi);
+
+	}
+
+}
