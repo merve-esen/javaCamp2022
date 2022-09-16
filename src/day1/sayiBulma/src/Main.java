f0ed1c0f6151ae8ceee7ae0774ff9a5d43359f5a
Merve Esen <merveesen834@gmail.com>
2022-09-16 10:41:14 +0300

    Day 1 - Assignment 2

---
+++ src/day1/sayiBulma/src/Main.java
@@ -0,0 +1,24 @@
+package day1.sayiBulma.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] sayilar = new int[] {1,2,5,7,8,9,0};
+		int aranacak = 5;
+		boolean varMi = false;
+
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+
+		if (varMi) {
+			System.out.println("Sayı mevcuttur");
+		} else {
+			System.out.println("Sayı mevcut değildir");
+		}
+	}
+
+}
