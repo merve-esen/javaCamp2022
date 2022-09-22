1e1e3571df1af568bd32635f2f93b4489648d826
Merve Esen <merveesen834@gmail.com>
2022-09-22 13:25:44 +0300

    Day 2 - Assignment 2

--- src/day2/methods2/src/Main.java
+++ src/day2/methods2/src/Main.java
@@ -8,6 +8,8 @@ public class Main {
 		System.out.println(yeniMesaj);
 		int sayi = topla(3,5);
 		System.out.println(sayi);
+		int toplam = topla2(3,5,8,11,18);
+		System.out.println(toplam);
 	}
 
 	public static void ekle() {
@@ -24,6 +26,14 @@ public class Main {
 
 	public static int topla(int sayi1, int sayi2) {
 		return sayi1 + sayi2;
+	}
+
+	public static int topla2(int...sayilar) {
+		int toplam = 0;
+		for (int sayi : sayilar) {
+			toplam += sayi;
+		}
+		return toplam;
 	}
 
 	public static String sehirVer() {
