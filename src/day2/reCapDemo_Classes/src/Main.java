4987cb6b9f6bcaffd6247a1bed2f0efdbf440e2a
Merve Esen <merveesen834@gmail.com>
2022-09-22 13:43:05 +0300

    Day 2 - Assignment 2

---
+++ src/day2/reCapDemo_Classes/src/Main.java
@@ -0,0 +1,11 @@
+package day2.reCapDemo_Classes.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		int sonuc = dortIslem.topla(4, 8);
+		System.out.println(sonuc);
+	}
+
+}
