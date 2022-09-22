e1b238b9af9d3f1e94a09edd627d163b3b999cac
Merve Esen <merveesen834@gmail.com>
2022-09-22 14:34:13 +0300

    Day 2 - Assignment 2

---
+++ src/day2/methodOverloading/src/Main.java
@@ -0,0 +1,11 @@
+package day2.methodOverloading.src;
+
+public class Main {
+
+	public static void main(String[] args) {
+		DortIslem dortIslem = new DortIslem();
+		System.out.println(dortIslem.topla(4, 8));
+		System.out.println(dortIslem.topla(3, 5, 10));
+	}
+
+}
